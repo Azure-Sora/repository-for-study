0706e9e6e13c2ffacf7e4423fdf7fd61d8e3a836
Azure-Sora <1918078227@qq.com>
2023-03-09 08:54:21 +0000

    3/9

---
+++ JavaLearning/src/ClassLearning/PolyTest/PolyTest.java
@@ -0,0 +1,9 @@
+package ClassLearning.PolyTest;
+
+public class PolyTest {
+    public static void main(String[] args) {
+        Person a = new Student();
+        //a.studentMethod();
+        a.personMethod();
+    }
+}
---
+++ JavaLearning/src/ClassLearning/PolyTest/Student.java
@@ -0,0 +1,9 @@
+package ClassLearning.PolyTest;
+
+public class Student extends Person{
+    public String ID;
+
+    public void studentMethod(){
+        System.out.println("student");
+    }
+}
