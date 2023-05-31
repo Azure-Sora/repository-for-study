6c471aa1de2eeb959a5a7391fd62463df394f277
Azure-Sora <1918078227@qq.com>
2023-05-31 10:55:48 +0800

    5/31

---
+++ JavaLearning/src/ForClass/JFrameTest/Main.java
@@ -0,0 +1,8 @@
+package ForClass.JFrameTest;
+
+public class Main {
+    public static void main(String[] args) {
+        new Test();
+
+    }
+}
---
+++ JavaLearning/src/ForClass/JFrameTest/Test.java
@@ -0,0 +1,10 @@
+package ForClass.JFrameTest;
+
+import javax.swing.*;
+
+public class Test {
+
+    private JPanel panel1;
+    private JLabel Lable1;
+    private JButton button1;
+}
