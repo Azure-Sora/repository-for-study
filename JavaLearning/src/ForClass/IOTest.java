0d7d083e0fe82aec825ea45eb2757f9602bfb464
Azure-Sora <1918078227@qq.com>
2023-06-14 09:47:51 +0800

    6/14

--- JavaLearning/src/ForClass/IOTest.java
+++ JavaLearning/src/ForClass/IOTest.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class IOTest {
     private static BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
@@ -11,13 +13,27 @@ public class IOTest {
 
     public static void main(String[] args) {
         String a = null;
+        Scanner sc = new Scanner(System.in);
         try {
             a = stdin.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        stdout.println(a);
-        String c = "aa";
-        String d = new String("aa");
+        StringTokenizer st = new StringTokenizer(a, " ");
+        String[] s = new String[0];
+        while (st.hasMoreTokens()) {
+            s = extendArray(s, 1);
+            s[s.length - 1] = st.nextToken();
+        }
+        for(String ele : s){
+            System.out.println(ele);
+        }
+
+    }
+
+    public static String[] extendArray(String[] src, int length) {
+        String[] newS = new String[src.length + length];
+        System.arraycopy(src, 0, newS, 0, src.length);
+        return newS;
     }
 }
