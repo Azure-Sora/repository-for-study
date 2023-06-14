d4ba960f586231d3827a14a77f739c22e573bb92
Azure-Sora <1918078227@qq.com>
2023-06-15 00:00:33 +0800

    6/14

---
+++ JavaLearning/src/ForClass/FileTest.java
@@ -0,0 +1,43 @@
+package ForClass;
+
+import java.io.*;
+
+public class FileTest {
+    public static void main(String[] args) {
+        PrintWriter stdout = new PrintWriter(System.out);
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        File file = new File("fileTest.txt");
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            } else {
+                file.delete();
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            FileWriter fw = new FileWriter(file);
+            for (; ; ) {
+                String input = stdin.readLine();
+                if (input.equals("")) {
+                    break;
+                }
+                fw.write(input + "\n");
+                fw.flush();
+            }
+            fw.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while (br.ready()) {
+                System.out.println(br.readLine());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
