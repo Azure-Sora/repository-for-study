5fd34361fea1a6d5418474ccb564e5a6e59980f8
Azure-Sora <1918078227@qq.com>
2023-10-23 14:05:26 +0800

    10/23

---
+++ 数据结构-Project1/Test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+template<class E>
+E add(E num1, E num2)
+{
+	return num1 + num2;
+}
+int main()
+{
+	cout << "slaves get your ass back here" << endl;
+	return 0;
+}
